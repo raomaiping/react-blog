6e1c2c9c56d81ace77b731d2466e44c6848f59da
2582395486@qq.com <2582395486@qq.com>
2020-03-15 15:30:36 +0800

    用List组件制作博客列表

--- pages/detailed.js
+++ pages/detailed.js
@@ -10,7 +10,7 @@ const Detailed = () => (
     <Header />
     <Row className="comm-main" type="flex" justify="center">
           <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-              左侧
+              
           </Col>
           <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
               右侧
