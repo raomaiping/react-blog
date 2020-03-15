4d3ff75c87f69bba1d853b3172d3cde293d708d3
2582395486@qq.com <2582395486@qq.com>
2020-03-15 16:37:10 +0800

    博客详情页面-编写基本结构

--- pages/detailed.js
+++ pages/detailed.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Head from 'next/head'
 import Header from '../components/Header'
-import {Row,Col} from 'antd'
+import Author from "../components/Author";
+import Advert from "../components/Advert";
+import Footer from "../components/Footer";
+import '../public/style/pages/detailed.css'
+import {Row,Col,Breadcrumb} from 'antd'
+import {CalendarOutlined,YoutubeOutlined,FireOutlined} from "@ant-design/icons";
 const Detailed = () => (
   <div>
     <Head>
@@ -10,12 +15,36 @@ const Detailed = () => (
     <Header />
     <Row className="comm-main" type="flex" justify="center">
           <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-              
+              <div>
+                <div className="bread-div">
+                  <Breadcrumb>
+                    <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a href="/list">视频教程</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a href="/">xxxx</a></Breadcrumb.Item>
+                  </Breadcrumb>
+                </div>
+                <div>
+                  <div className="detailed-title">
+                      react实战视频教程
+                  </div>
+                  <div className="list-icon center">
+                    <span><CalendarOutlined /> 2020-03-15</span>
+                    <span><YoutubeOutlined /> 视频教程</span>
+                    <span><FireOutlined /> 9999人</span>
+                  </div>
+                  <div className="detailed-content">
+                        markdown  内容
+                  </div>
+                </div>
+              </div>
           </Col>
           <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-              右侧
+            <Author />
+            <Advert />
           </Col>
     </Row>
+
+    <Footer />
   </div>
 )
 
