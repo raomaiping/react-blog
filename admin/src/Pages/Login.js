dc10f88ebd93581056520b6358bec3ba40efc6b5
2582395486@qq.com <2582395486@qq.com>
2020-03-16 23:41:37 +0800

    登录页UI制作

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,9 +1,45 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import '../static/css/Login.css'
+import {Card,Input,Button,Spin} from 'antd'
+import { createFromIconfontCN } from '@ant-design/icons';
+import 'antd/dist/antd.css'
 function Login(){
+    const IconFont = createFromIconfontCN({
+        scriptUrl: '//at.alicdn.com/t/font_1694685_d4cx4au3fp.js',
+      });
+    const [userName,setUserName] = useState('')
+    const [passWord,setPassWord] = useState('')
+    const [isLoading,setIsLoading] = useState(false)
+
+    const checkLogin = ()=>{
+        setIsLoading(true)
+        setTimeout(()=>{
+            setIsLoading(false)
+        },1000)
+    }
     return(
-        <div>
-            我是登录页面
+        <div className="login-div">
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="大帅哥的博客后台" bordered={true} style={{width:400}}>
+                    <Input 
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<IconFont type="icon--MaleUser" />}
+                        onChange={e=>{setUserName(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <Input.Password 
+                        id="passWord"
+                        size="large"
+                        placeholder="Enter your passWord"
+                        prefix={<IconFont type="icon-key" />}
+                        onChange={e=>{setPassWord(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <Button type="primary" size="large" block onClick={checkLogin}>Login</Button>
+                </Card>
+            </Spin>
         </div>
     )
 }
