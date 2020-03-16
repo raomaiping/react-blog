787a8a455f05e648debef1cc0ef131633ab8a13c
2582395486@qq.com <2582395486@qq.com>
2020-03-16 22:38:58 +0800

    后台开发-搭建开发环境

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,17 +1,40 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import axios from 'axios'
-import Link from 'next/link'
+import axios from "axios";
+import Link from "next/link";
 import Header from "../components/Header";
 import Author from "../components/Author";
 import Advert from "../components/Advert";
 import Footer from "../components/Footer";
 import { Row, Col, List } from "antd";
-import {CalendarOutlined,YoutubeOutlined,FireOutlined } from "@ant-design/icons";
-import '../public/style/pages/index.css'
-import servicePath from '../config/apiUrl'
-const Home = (list) => {
+import {
+  CalendarOutlined,
+  YoutubeOutlined,
+  FireOutlined
+} from "@ant-design/icons";
+import "../public/style/pages/index.css";
+import servicePath from "../config/apiUrl";
+import marked from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
+const Home = list => {
   const [mylist, setMylist] = useState(list.data);
+  const renderer = new marked.Renderer();
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    sanitize: false,
+    xhtml: false,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
   return (
     <div>
       <Head>
@@ -27,18 +50,28 @@ const Home = (list) => {
             renderItem={item => (
               <List.Item>
                 <div className="list-title">
-                  <Link href={{pathname:'/detailed',query:{id:item.id}}} >
-                    <a>
-                      {item.title}
-                    </a>
+                  <Link
+                    href={{ pathname: "/detailed", query: { id: item.id } }}
+                  >
+                    <a>{item.title}</a>
                   </Link>
                 </div>
                 <div className="list-icon">
-                  <span><CalendarOutlined />{item.addTime}</span>
-                  <span><YoutubeOutlined /> {item.typeName}</span>
-                  <span><FireOutlined /> {item.view_count}人</span>
+                  <span>
+                    <CalendarOutlined />
+                    {item.addTime}
+                  </span>
+                  <span>
+                    <YoutubeOutlined /> {item.typeName}
+                  </span>
+                  <span>
+                    <FireOutlined /> {item.view_count}人
+                  </span>
                 </div>
-                <div className="list-context">{item.introduce}</div>
+                <div
+                  className="list-context"
+                  dangerouslySetInnerHTML={{ __html: marked(item.introduce) }}
+                ></div>
               </List.Item>
             )}
           />
@@ -48,19 +81,18 @@ const Home = (list) => {
           <Advert />
         </Col>
       </Row>
-      
+
       <Footer />
     </div>
   );
 };
 
-Home.getInitialProps = async ()=>{
-  const promise = new Promise((reslove) =>{
-    axios(servicePath.getArticleList)
-    .then((res)=>{
-      reslove(res.data)
-    })
-  })
-  return promise
-}
+Home.getInitialProps = async () => {
+  const promise = new Promise(reslove => {
+    axios(servicePath.getArticleList).then(res => {
+      reslove(res.data);
+    });
+  });
+  return promise;
+};
 export default Home;
