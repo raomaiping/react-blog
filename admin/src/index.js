0e9dafbd037b6de69f9af63fa70eb72832d94c40
2582395486@qq.com <2582395486@qq.com>
2020-03-16 22:49:09 +0800

    后台开发-页面路由配置

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Main from './Pages/Main';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 
